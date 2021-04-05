1c56661962b78a5100c053d85bdbed0a0682ece3
Carlos Sergio <60165621+carlos-sergio-carvalho@users.noreply.github.com>
2021-04-05 15:55:10 -0300

    porta 5000

--- CalendarioGerarOrg/Models/AppDbContext.cs
+++ CalendarioGerarOrg/Models/AppDbContext.cs
@@ -29,7 +29,10 @@ namespace CalendarioGerarOrg.Models
                 //.UseMySql("Server=six-sql2014;Database=calendariogerar;Uid=root;Pwd=six.2009;");
                 //.UseMySql("Server=mysql.www2.sixtech.com.br;Database=www2;User Id=www2;Password=six2009;");
                 .UseMySql("Server=node66382-gerarcalendario.jelastic.saveincloud.net;Database=calendario;User Id=root;Password=3UGHxW6Wur;",
-                new MySqlServerVersion(new Version(5, 7, 32)));
+                new MySqlServerVersion(new Version(5, 7, 32))
+                , mySqlOptions => mySqlOptions
+                             .CharSetBehavior(Pomelo.EntityFrameworkCore.MySql.Infrastructure.CharSetBehavior.NeverAppend)
+                );
         }
 
         
