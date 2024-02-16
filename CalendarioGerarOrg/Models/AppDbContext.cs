520207b77569da3faae68e428c5dbda6b9985ae8
Sergio <sergio@sixtech.com.br>
2024-02-16 15:41:50 -0300

    hot DB correto para teste

--- CalendarioGerarOrg/Models/AppDbContext.cs
+++ CalendarioGerarOrg/Models/AppDbContext.cs
@@ -22,13 +22,20 @@ namespace CalendarioGerarOrg.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-
+            string hostName = System.Net.Dns.GetHostName();
+            if(hostName.Contains("teste") || System.Diagnostics.Debugger.IsAttached) 
+            {
+                hostName = "node91570-gerarcalendario-teste630161.jelastic.saveincloud.net";
+            }
+            else {
+                hostName = "node66382-gerarcalendario.jelastic.saveincloud.net";
+            }
             //optionsBuilder.UseSqlite($"Filename={App._dbPath}");
             optionsBuilder
                 .UseLazyLoadingProxies()
                 //.UseMySql("Server=six-sql2014;Database=calendariogerar;Uid=root;Pwd=six.2009;");
                 //.UseMySql("Server=mysql.www2.sixtech.com.br;Database=www2;User Id=www2;Password=six2009;");
-                .UseMySql("Server=node66382-gerarcalendario.jelastic.saveincloud.net;Database=calendario;User Id=root;Password=3UGHxW6Wur;",
+                .UseMySql($"Server={hostName};Database=calendario;User Id=root;Password=3UGHxW6Wur;",                
                 new MySqlServerVersion(new Version(5, 7, 32))
                 , mySqlOptions => mySqlOptions
                              .CharSetBehavior(Pomelo.EntityFrameworkCore.MySql.Infrastructure.CharSetBehavior.NeverAppend)
